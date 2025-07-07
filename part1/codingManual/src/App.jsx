dfdbe9266d4298be9e37a10f8a2fc5be54143e62
samid <sabarrera21@ucatolica.edu.co>
2025-07-07 11:54:26 -0500

    Desestructuración de props

---
+++ part1/codingManual/src/App.jsx
@@ -0,0 +1,40 @@
+const  Hello = ({name,age}) => {
+
+     const bornYear = ()=>{
+         const yearNow = new Date().getFullYear();
+         return yearNow - age
+     }
+
+    return (
+        <div>
+            <p>
+                Hello {name}, you are {age} years old.
+            </p>
+            <p>
+                So you were probably born in {bornYear()}
+            </p>
+        </div>
+    )
+}
+
+const  Footer= () =>{
+    return (
+        <div>
+            greating app created by <a href='https://github.com/mluukkay'>mluukkay</a>
+
+        </div>
+    )
+}
+const  App = () => {
+    const  name = 'Peter'
+    const  age =10
+
+  return (
+      <div>
+          <h1>Greetings</h1>
+          <Hello name='Maya' age={26+10}/>
+          <Hello name={name} age={age}/>
+      </div>
+  )
+}
+export default App
