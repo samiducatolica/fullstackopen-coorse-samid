16b9dfc57e0e66bd32af6cedf4c94c17e2153c5d
samid <sabarrera21@ucatolica.edu.co>
2025-07-07 23:09:35 -0500

    Pasando el estado a componentes hijos

--- part1/codingManual/src/App.jsx
+++ part1/codingManual/src/App.jsx
@@ -1,20 +1,46 @@
 import {useState} from 'react'
 
+const Display = (props) => {
+    return (
+        <div>{props.counter}</div>
+    )
+}
+
+
+const Button = (props) => {
+    return (
+        <button onClick={props.onClick}>
+            {props.text}
+        </button>
+    )
+}
+
+
+
 const App = () => {
     const [counter, setCounter] = useState(0)
 
     const increaseByOne = () => setCounter(counter + 1)
+    const decreaseByOne = () => setCounter(counter - 1)
     const setZero = () => setCounter(0)
 
     return (
         <div>
-            {counter}
-            <button onClick={increaseByOne}>
-                plus
-            </button>
-            <button onClick={setZero}>
-                zero
-            </button>
+            <h2>Passando el estado a componentes hijos</h2>
+            <Display counter={counter}/>
+            <Button
+                onClick={increaseByOne}
+                text='plus'
+            />
+            <Button
+                onClick={setZero}
+                text='zero'
+            />
+
+            <Button
+                onClick={decreaseByOne}
+                text='minus'
+            />
         </div>
     )
 }
