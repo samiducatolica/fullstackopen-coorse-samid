96d67fb1fdf2a96dfeab63029a5c8ebec87e5b90
samid <sabarrera21@ucatolica.edu.co>
2025-07-07 22:54:17 -0500

    Controlador de eventos en una funcion

--- part1/codingManual/src/App.jsx
+++ part1/codingManual/src/App.jsx
@@ -1,44 +1,20 @@
 import {useState} from 'react'
 
-const Hello = ({name, age}) => {
-
-    const bornYear = () => {
-        const yearNow = new Date().getFullYear();
-        return yearNow - age
-    }
-
-    return (
-        <div>
-            <p>
-                Hello {name}, you are {age} years old.
-            </p>
-            <p>
-                So you were probably born in {bornYear()}
-            </p>
-        </div>
-    )
-}
-
-const Footer = () => {
-    return (
-        <div>
-            greating app created by <a href='https://github.com/mluukkay'>mluukkay</a>
-
-        </div>
-    )
-}
 const App = () => {
     const [counter, setCounter] = useState(0)
 
-    setTimeout(() => {
-        setCounter(counter + 1)
-    },1000)
-
-    console.log('rendering...',counter)
+    const increaseByOne = () => setCounter(counter + 1)
+    const setZero = () => setCounter(0)
 
     return (
         <div>
             {counter}
+            <button onClick={increaseByOne}>
+                plus
+            </button>
+            <button onClick={setZero}>
+                zero
+            </button>
         </div>
     )
 }
