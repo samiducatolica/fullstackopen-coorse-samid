d0f7df1868ad47930cc567b1ab4845539d0fec01
samid <sabarrera21@ucatolica.edu.co>
2025-07-11 14:19:39 -0500

    La actualización del estado es asíncrona

--- part1/codingManual/src/App.jsx
+++ part1/codingManual/src/App.jsx
@@ -11,29 +11,35 @@ const App = () => {
     const [left, setLeft] = useState(0)
     const [right, setRight] = useState(0)
     const [allClicks, setAll] = useState([])
+    const [total, setTotal] = useState(0)
 
     const handleLeftClick = () =>{
         setAll(allClicks.concat('L'))
-        setLeft(left +1)
+        const updateLeft= left +1
+        setLeft(updateLeft)
+        setTotal(updateLeft +right)
     }
 
 
 
     const handleRightClick = () =>{
         setAll(allClicks.concat('R'))
-        setRight(right +1)
+        const updateRight= right+1
+        setRight(updateRight)
+        setTotal(left +updateRight)
     }
 
 
 
     return (
         <div>
-            <h2>Manejo de arrays</h2>
+            <h2>La actualización del estado es asíncrona</h2>
             {left}
             <button onClick={handleLeftClick}>left</button>
             <button onClick={handleRightClick}>right</button>
             {right}
             <p>{allClicks.join(' ')}</p>
+            <p>Total de clicks: {total}</p>
         </div>
     )
 }
