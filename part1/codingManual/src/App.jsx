b5078c551fce47ae35493e0db802968ec4b48961
samid <sabarrera21@ucatolica.edu.co>
2025-07-11 13:48:52 -0500

    Manejo de arrays

--- part1/codingManual/src/App.jsx
+++ part1/codingManual/src/App.jsx
@@ -7,26 +7,33 @@ const Button = ({onClick, text}) => (<button onClick={onClick}>{text}</button>)
 
 
 const App = () => {
-    const [clicks,setClicks] = useState(
-    {
-        left: 0, right: 0
-    })
 
-    const handleLeftClick = () =>
-        setClicks({...clicks, left: clicks.left + 1})
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+
+    const handleLeftClick = () =>{
+        setAll(allClicks.concat('L'))
+        setLeft(left +1)
+    }
 
 
-    const handleRightClick = () =>
-        setClicks({...clicks, right: clicks.right + 1})
+
+    const handleRightClick = () =>{
+        setAll(allClicks.concat('R'))
+        setRight(right +1)
+    }
+
 
 
     return (
         <div>
-            <h2>Estado complejo</h2>
-            {clicks.left}
+            <h2>Manejo de arrays</h2>
+            {left}
             <button onClick={handleLeftClick}>left</button>
             <button onClick={handleRightClick}>right</button>
-            {clicks.right}
+            {right}
+            <p>{allClicks.join(' ')}</p>
         </div>
     )
 }
