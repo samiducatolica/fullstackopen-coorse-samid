8f581ea75c07909078cd0c3ff6433abf00b56adc
samid <sabarrera21@ucatolica.edu.co>
2025-07-13 17:42:27 -0500

    Una funcion que retorna una funcion

--- part1/codingManual/src/App.jsx
+++ part1/codingManual/src/App.jsx
@@ -2,14 +2,16 @@ import {useState} from 'react'
 
 // const Display = ({counter}) => <div>{counter}</div>
 
-const Button = ({handleClick, text}) => (
-    <button onClick={handleClick}>{text}</button>
-)
+const Button = ({handleClick, text}) => {
+    return (
+        <button onClick={handleClick}>{text}</button>
+    )
+}
 
-const History = (props)=> {
+const History = (props) => {
 
     if (props.allClicks.length === 0) {
-        return(
+        return (
             <div>
                 The app is used by pressing the buttons.
             </div>
@@ -25,35 +27,21 @@ const History = (props)=> {
 
 const App = () => {
 
-    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
-    const [allClicks, setAll] = useState([])
-    const [total, setTotal] = useState(0)
+    const [value, setValue] = useState(10)
 
-    const handleLeftClick = () =>{
-        setAll(allClicks.concat('L'))
-        const updateLeft= left +1
-        setLeft(updateLeft)
-        setTotal(updateLeft +right)
+    const setToValue = (newValue) => () =>{
+            console.log('value now',newValue)
+            setValue(newValue)
     }
 
-    const handleRightClick = () =>{
-        setAll(allClicks.concat('R'))
-        const updateRight= right+1
-        setRight(updateRight)
-        setTotal(updateRight +left)
-
-    }
 
     return (
         <div>
-            <h2>Renderizado condicional</h2>
-            {left}
-            <Button handleClick={handleLeftClick} text='left'/>
-            <Button handleClick={handleRightClick} text='right'/>
-            {right}
-            <p>{total}</p>
-            <History allClicks={allClicks}/>
+            {value}
+            <h2>Una funcion que retorna una funcion</h2>
+            <button onClick={setToValue(1000)}>Thousand</button>
+            <button onClick={setToValue(0)}>Reset</button>
+            <button onClick={setToValue(value+1)}>Increment</button>
         </div>
     )
 }
