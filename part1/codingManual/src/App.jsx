9388799c8f51b4533dedc640e4a28aaf6b8acbc0
samid <sabarrera21@ucatolica.edu.co>
2025-07-13 18:25:43 -0500

    No definir componentes dentro de los componentes

--- part1/codingManual/src/App.jsx
+++ part1/codingManual/src/App.jsx
@@ -1,29 +1,14 @@
 import {useState} from 'react'
 
-// const Display = ({counter}) => <div>{counter}</div>
+//Este el el lugar para definir componentes
 
-const Button = (props) => {
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
     <button onClick={props.handleClick}>
         {props.text}
     </button>
-}
-
-const History = (props) => {
-
-    if (props.allClicks.length === 0) {
-        return (
-            <div>
-                The app is used by pressing the buttons.
-            </div>
-        )
-    }
-
-    return (
-        <div>
-            Button press history: {props.allClicks.join(' ')}
-        </div>
-    )
-}
+)
 
 const App = () => {
 
@@ -34,14 +19,26 @@ const App = () => {
         setValue(newValue)
     }
 
+    //No definas componentes adentro de otro componente
 
     return (
         <div>
-            {value}
+            <Display value={value} />
             <h2>Pasando controladores de eventos a componentes hijos</h2>
-            <button onClick={() => setToValue(1000)}>Thousand</button>
-            <button onClick={() => setToValue(0)}>Reset</button>
-            <button onClick={() => setToValue(value + 1)}>Increment</button>
+            <Button handleClick={() => setToValue(1000)} text="thousand" />
+            <Button handleClick={() => setToValue(0)} text="reset" />
+            <Button handleClick={() => setToValue(value +1)} text="increment" />
+
+            <h2>Juramento de los programadores</h2>
+            <p>Programar es difícil, por eso usaré todos los medios posibles para hacerlo más fácil.</p>
+            <ul>
+                <li>Tendre la consola de desarrollado de mi navegador abierta todo el tiempo</li>
+                <li>Progreso con pequeños pasos.</li>
+                <li>Escribiré muchas declaraciones console.log para asegurarme de que entiendo cómo se comporta el código y para ayudar a identificar problemas</li>
+                <li>Si mi código no funciona, no escribiré más código. En lugar de eso, empiezo a eliminar el código hata que funcione o simplemente vuelvo a un estado en el que todo seguía funcionando.</li>
+                <li>Cuando pido ayuda en el canal de Discord del curso o en otro lugar, formulo mis preguntas correctamente.</li>
+            </ul>
+
         </div>
     )
 }
