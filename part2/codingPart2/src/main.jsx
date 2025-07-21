028d1616fc59f92856d858a61f19b6d3be31c83e
samid <sabarrera21@ucatolica.edu.co>
2025-07-21 17:49:33 -0500

    Codigo de part2-a

--- part2/codingPart2/src/main.jsx
+++ part2/codingPart2/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React  from "react";
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const notes = [
+    {
+        id: 1,
+        content: 'HTML is easy',
+        importat: true
+    },
+    {
+        id: 2,
+        content: 'Browser can only execute JavaScript',
+        importat: false
+    },
+    {
+        id: 3,
+        content: 'GET and POST are the most important methods of HTTP protocol',
+        importat: true
+    },
+]
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App notes={notes}/>
+);
