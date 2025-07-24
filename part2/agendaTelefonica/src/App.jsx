209ed4d49ea3a894a8879ba5a01c8ebbc200266e
samid <sabarrera21@ucatolica.edu.co>
2025-07-24 09:42:14 -0500

    Agenda telefonica paso 3

--- part2/agendaTelefonica/src/App.jsx
+++ part2/agendaTelefonica/src/App.jsx
@@ -3,37 +3,49 @@ import {useState} from "react";
 const Name = (props) =>{
     return (
         <div>
-            <p>{props.name}</p>
+            <p>{props.name} - {props.telefono} </p>
+
         </div>
     )
 }
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: 'Samid Amaury'},
+        {name: 'Samid Amaury', telefono:'123456789'},
     ])
 
     const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
 
+    //Manejadores de eventos
+    // Nombre
     const handleNameChange = (event) => {
-        console.log(event.target.value)
+        // console.log(event.target.value)
         setNewName(event.target.value)
     }
+    //Telefono
+    const handleNumberChange = (event)=>{
+        console.log(event.target.value)
+        setNewNumber(event.target.value)
+    }
+    //Envio y validacion de formulario
     const handleNameSubmit = (event) => {
         event.preventDefault();
         const NewPerson = {
-            name: newName
+            name: newName,
+            telefono:newNumber
         }
-
+        //variable de validacion
         const validation = persons.findIndex(p => p.name == NewPerson.name);
 
-        //existing person check
+        //validacion de nombre
         if(validation != -1){
             alert(`${NewPerson.name} ya existe en el ditectorio`);
         }else {
             //crea nuevo contacto
             setPersons(persons.concat(NewPerson));
             setNewName('')
+            setNewNumber('');
         }
     }
 
@@ -48,13 +60,19 @@ const App = () => {
                 />
                 </div>
                 <div>
+                    telefono: <input
+                    value={newNumber}
+                    onChange={handleNumberChange}
+                />
+                </div>
+                <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
             <div><strong>Debug</strong>:
                 {persons.map((person) => (
-                    <Name key={person.name} name={person.name}/>
+                    <Name key={person.name} name={person.name} telefono={person.telefono} />
                 ))}
             </div>
 
