522857336da48c6bf273ebe405681e55f9f6c492
samid <sabarrera21@ucatolica.edu.co>
2025-07-24 12:36:26 -0500

    2.10 Agenda telefonica paso 5

--- part2/agendaTelefonica/src/App.jsx
+++ part2/agendaTelefonica/src/App.jsx
@@ -1,5 +1,38 @@
 import {useState} from "react";
 
+const FormNewPerson = (props) => {
+    return (
+        <form onSubmit={props.handleNameSubmit}>
+            <div>
+                name: <input
+                value={props.newName}
+                onChange={props.handleNameChange}
+            />
+            </div>
+            <div>
+                telefono: <input
+                value={props.newNumber}
+                onChange={props.handleNumberChange}
+            />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    )
+}
+
+const Filter = (props) => {
+    return (
+        <div>
+            Filtrar por nombre: <input
+            value={props.findName}
+            onChange={props.handleFindChange}
+        />
+        </div>
+    )
+}
+
 const Name = (props) => {
     // console.log('Comp Name', props);
     return (
@@ -10,22 +43,23 @@ const Name = (props) => {
     )
 }
 
-const FilteredPersons = ({persons, findName}) => {
+const PersonsList = ({persons, findName}) => {
     if (!findName) {
         // console.log('No hay nombre para filtrar');
         // console.log('FilteredPersons', persons, findName);
         return (
             <div>
+                <strong>Debug</strong>:
                 {persons.map((person) => (<Name key={person.id} name={person.name} telefono={person.telefono}/>))}
             </div>
         )
     } else {
         console.log('Filtrando por nombre:', findName);
-        const filteredPersons = persons.
-        filter(elemento => elemento.name.toLowerCase().indexOf(findName.toLowerCase()) > -1)
+        const filteredPersons = persons.filter(elemento => elemento.name.toLowerCase().indexOf(findName.toLowerCase()) > -1)
         return (
-            <div>
-                {filteredPersons.map((person) => (<Name key={person.id} name={person.name} telefono={person.telefono}/>))}
+            <div><strong>Debug</strong>:
+                {filteredPersons.map((person) => (
+                    <Name key={person.id} name={person.name} telefono={person.telefono}/>))}
             </div>
         )
     }
@@ -33,11 +67,11 @@ const FilteredPersons = ({persons, findName}) => {
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: 'Samid', telefono: '123456789', id:1},
-        {name: 'Amaury', telefono: '123456789', id:2},
-        {name: 'Jose', telefono: '123456789', id:3},
-        {name: 'Aberto', telefono: '123456789', id:4},
-        {name: 'Maria', telefono: '123456789', id:5},
+        {name: 'Samid', telefono: '123456789', id: 1},
+        {name: 'Amaury', telefono: '123456789', id: 2},
+        {name: 'Jose', telefono: '123456789', id: 3},
+        {name: 'Aberto', telefono: '123456789', id: 4},
+        {name: 'Maria', telefono: '123456789', id: 5},
     ])
 
     const [newName, setNewName] = useState('');
@@ -72,10 +106,10 @@ const App = () => {
             id: persons.length + 1
         }
         //variable de validacion
-        const validation = persons.findIndex(p => p.name == NewPerson.name);
+        const validation = persons.findIndex(p => p.name === NewPerson.name);
 
         //validacion de nombre
-        if (validation != -1) {
+        if (validation !== -1) {
             alert(`${NewPerson.name} ya existe en el ditectorio`);
         } else {
             //crea nuevo contacto
@@ -88,34 +122,18 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
-            <div>
-                Filtrar por nombre: <input
-                value={findName}
-                onChange={handleFindChange}
+            <Filter findName={findName} handleFindChange={handleFindChange}/>
+            <h2>Agregar un nuevo registro</h2>
+            <FormNewPerson
+                newName={newName}
+                newNumber={newNumber}
+                handleNameChange={handleNameChange}
+                handleNumberChange={handleNumberChange}
+                handleNameSubmit={handleNameSubmit}
             />
-            </div>
-            <h2>Agrega un nuevo registro</h2>
-            <form onSubmit={handleNameSubmit}>
-                <div>
-                    name: <input
-                    value={newName}
-                    onChange={handleNameChange}
-                />
-                </div>
-                <div>
-                    telefono: <input
-                    value={newNumber}
-                    onChange={handleNumberChange}
-                />
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
             <h2>Numbers</h2>
-            <div><strong>Debug</strong>:
-                <FilteredPersons persons={persons} findName={findName}/>
-            </div>
+            <PersonsList persons={persons} findName={findName}/>
+
 
         </div>
     )
