4e48c96db595fdad7f17a4cbe7780e8f2c24fd9a
samid <sabarrera21@ucatolica.edu.co>
2025-07-25 16:38:04 -0500

    promesas y axios  para consultas al servidor

--- part2/codingPart2/src/main.jsx
+++ part2/codingPart2/src/main.jsx
@@ -1,25 +1,7 @@
-import React  from "react";
+import React from "react";
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        importat: true
-    },
-    {
-        id: 2,
-        content: 'Browser can only execute JavaScript',
-        importat: false
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        importat: true
-    },
-]
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App notes={notes}/>
-);
+    <App/>
+)
