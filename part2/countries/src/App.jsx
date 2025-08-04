dd934c9fcc6186b40473b6207a6be5db1c1e552e
samid <sabarrera21@ucatolica.edu.co>
2025-08-04 17:59:59 -0500

    2.20 Datos de paises, paso3

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react'
 import countriServices from "./services/countriServices.js";
+import weatherServices from "./services/weatherServices.js";
 
 
 const CountriList = ({search, countries, handleButton}) => {
@@ -15,7 +16,7 @@ const CountriList = ({search, countries, handleButton}) => {
     } else {
         const filterdCounties = countries.filter(e => e.name.common.toLowerCase().indexOf(search.toLowerCase()) !== -1);
         if (filterdCounties.length > 10) {
-            console.log('filteredCounties', filterdCounties);
+            // console.log('filteredCounties', filterdCounties);
             return (
                 <div>
                     <p>Too many matches, specify another filter.</p>
@@ -37,30 +38,76 @@ const CountriList = ({search, countries, handleButton}) => {
             )
         } else if (filterdCounties.length === 1) {
             const country = filterdCounties[0];
-            console.log('country', country);
-            return(
+            return (
                 <div>
-                    <h1>{country.name.common}</h1>
-                    <p>Oficial Name: <strong>{country.name.official}</strong></p>
-                    <p>capital: {country.capital[0]}</p>
-                    <p>area: {country.area} Km</p>
-                    <h2>Languages</h2>
-                    <ul>
-                        {Object.values(country.languages).map((language, index) => (
-                            <li key={index}>{language}</li>
-                        ))}
-                    </ul>
-                    <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="200"/>
-
+                    <CountriInfo country={country}/>
                 </div>
             )
         }
     }
 }
 
-const CountriButton =({country, onClick}) => {
+const CountriInfo = ({country}) => {
+    // console.log(country)
+    return (
+        <div>
+            <h1>{country.name.common}</h1>
+            <p>Oficial Name: <strong>{country.name.official}</strong></p>
+            <p>capital: {country.capital[0]}</p>
+            <p>area: {country.area} Km</p>
+            <h2>Languages</h2>
+            <ul>
+                {Object.values(country.languages).map((language, index) => (
+                    <li key={index}>{language}</li>
+                ))}
+            </ul>
+            <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="200"/>
+            <h2>Weather in {country.capital[0]}</h2>
+            <WeatherInfo city={country.capital[0]}/>
+        </div>
+    )
+}
+
+const WeatherInfo = ({city}) => {
+    const [temperature, setTemperature] = useState(null);
+    const [wind, setWind] = useState(null);
+    const [icon, setIcon] = useState(null);
+
+    useEffect(() => {
+        if(!city) return;
+        weatherServices.getWeatherCity(city)
+            .then(weather => {
+                console.log('Weather data:', weather);
+                setTemperature((weather.main.temp- 273.15).toFixed(2)); // Convert Kelvin to Celsius
+                setWind(weather.wind.speed);
+                setIcon(weather.weather[0].icon);
+            })
+            .catch(error => {
+                console.error('Error fetching weather data:', error);
+            });
+    }, [city]);
+
+    return (
+        <div>
+            {temperature !== null
+                ? <p>Temperatura: {temperature} °C</p>
+                : <p>Cargando temperatura...</p>
+            }
+            {icon &&
+                <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+                     alt={`weather icon for ${city} city`}
+                />
+            }
+            {wind !== null
+                ? <p>Viento: {wind} m/s</p>
+                : <p>Cargando viento...</p>
+            }
+        </div>
+    );
+
 
 }
+
 
 function App() {
     const [countriList, setcountriList] = useState(null)
@@ -80,7 +127,6 @@ function App() {
         setSearch(event.target.value)
     }
     const handleButton = (country) => {
-        console.log('id', country.name.common);
         setSearch(country.name.common);
         document.getElementById('countri').value = country.name.common;
     }
