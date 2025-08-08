369a87a07643cf04ce2d0bb69eeecf9555f57b77
samid <sabarrera21@ucatolica.edu.co>
2025-08-08 14:42:57 -0500

    3.2 Backend de la Agenda Telefónica, paso 2

---
+++ part3/AgendaTelefonicaBackend/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express()
+
+let persons = [
+    {
+        "id": 1, "name": "Arto Hellas", "number": "040-123456"
+    }, {
+        "id": 2, "name": "Ada Lovelace", "number": "39-44-5323523"
+    }, {
+        "id": 3, "name": "Dan Abramov", "number": "12-43-234345"
+    }, {
+        "id": 4, "name": "Mary Poppendieck", "number": "39-23-6423122"
+    }
+]
+app.get('/api/persons',(request,response)=>{
+    console.log('GET /api/persons');
+    response.json(persons)
+})
+
+app.get('/info',(request,response)=>{
+    const peronsTotal= persons.length
+    const hoy=new Date().toString()
+    console.log(hoy)
+    response.send(
+        `<p>Phonebook has info for ${peronsTotal} persons</p> 
+                <p>${hoy}</p>`
+    )
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Hello Server runing on port ${PORT}`)
